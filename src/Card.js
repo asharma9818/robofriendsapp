0ace0a9ab9c08bed77deb9670a613af9a2f85bd0
Abhishek Sharma <abhishek777sharma@gmail.com>
2021-08-30 16:38:07 -0400

    1. created a CardList component in index..js which passes robots as props

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Card = (props) => {
-    const { name, email, id} = props;  // destructuring 
+const Card = ({ name, email, id }) => {
+    
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robot' src={`https://robohash.org/robo1/${id}`} /> {/* ES6 template strings*/}  
+            <img alt='robot' src={`https://robohash.org/robo1/${id}`} /> {/* ES6 template strings*/}
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
