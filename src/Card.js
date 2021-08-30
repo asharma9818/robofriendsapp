e673330a197361bacfca3b9c5e1064aff7ee2b59
Abhishek Sharma <abhishek777sharma@gmail.com>
2021-08-30 14:26:51 -0400

    added tachyons for styling. adding robots.js with multiple users. Enhancing Card component function using props and destructuring. Updating image src with id using ES6 template strings

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
+    const { name, email, id} = props;  // destructuring 
     return (
-        <div>
-            <img alt='robot' src='https://robohash.org/robo1' /> 
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='robot' src={`https://robohash.org/robo1/${id}`} /> {/* ES6 template strings*/}  
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
