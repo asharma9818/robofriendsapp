5cef11a41254b0d70d2efdca454cd034ab4881a5
Abhishek Sharma <abhishek777sharma@gmail.com>
2021-09-01 00:46:44 -0400

    Added errorboundry to handle any errors in the app

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 // import { robots } from './robots';
 import SearchBox from '../components/SearchBox';
-import './App.css';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
 
 class App extends Component {
     constructor() {
@@ -38,7 +39,9 @@ class App extends Component {
                 <h1 className='f1'>RoboFriendsApp</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
+                    <ErrorBoundry>
                     <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
                 </Scroll>
             </div>
         ));
